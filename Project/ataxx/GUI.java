5341e10deb1348b9a2a0f7aa147263df675eaa9f
yilin <yyl956403777@163.com>
2023-05-17 21:49:16 +0800

    gui

--- Project/ataxx/GUI.java
+++ Project/ataxx/GUI.java
@@ -1,18 +1,72 @@
 package ataxx;
 
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+import java.io.InputStream;
+import java.awt.Menu;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.BorderFactory;
+import static ataxx.PieceState.*;
+import static ataxx.Utils.*;
+
 // Optional Task: The GUI for the Ataxx Game
 
 class GUI implements View, CommandSource, Reporter {
-
+    private Board board;
+    private final ArrayBlockingQueue<String> _commandQueue =
+    new ArrayBlockingQueue<>(5);
     // Complete the codes here
+    /**
+     * @param ataxx
+     */
     GUI(String ataxx) {
-
+        addMenuButton("Game->New", this::newGame);
+        addMenuRadioButton("Game->Blocks->Set Blocks", "Blocks",
+                           false, this::adjustBlockMode);
+        addMenuRadioButton("Game->Blocks->Move Pieces", "Blocks",
+                           true, this::adjustBlockMode);
+        addMenuButton("Game->Quit", this::quit);
+        addMenuButton("Options->Seed...", this::setSeed);
+        addMenuRadioButton("Options->Players->Red AI", "Red",
+                           false, (dummy) -> send("auto red"));
+        addMenuRadioButton("Options->Players->Red Manual", "Red",
+                           true, (dummy) -> send("manual red"));
+        addMenuRadioButton("Options->Players->Blue AI", "Blue",
+                           true, (dummy) -> send("auto blue"));
+        addMenuRadioButton("Options->Players->Blue Manual", "Blue",
+                           false, (dummy) -> send("manual blue"));
+        addMenuButton("Info->Help", this::doHelp);
+        _widget = new BoardWidget(_commandQueue);
+        add(_widget,
+            new LayoutSpec("height", "1",
+                           "width", "REMAINDER",
+                           "ileft", 5, "itop", 5, "iright", 5,
+                           "ibottom", 5));
+        addLabel("Red to move", "State",
+                 new LayoutSpec("y", 1, "anchor", "west"));
+        addButton("Pass", this::doPass, new LayoutSpec("y",
     }
-
     // Add some codes here
 
 
-
+    
 
 
 
@@ -20,32 +74,60 @@ class GUI implements View, CommandSource, Reporter {
 	
     @Override
     public void update(Board board) {
-
+        Game game = new Game(this, this, this);
+        game.runCommand("board_on");
     }
 
     @Override
     public String getCommand(String prompt) {
-        return null;
+        try {
+            return _commandQueue.take();
+        } catch (InterruptedException excp) {
+            throw new Error("unexpected interrupt");
+        }
     }
 
     @Override
-    public void announceWinner(PieceState state) {
-
+    public void announceWinner(PieceState a) {
+//FIXME
+if (board.getColorNums(PieceState.RED) == 0) {
+    JOptionPane.showMessageDialog(null, "BLUE WINS! We have a winner String");
+   
+if (board.getColorNums(PieceState.BLUE) == 0) {
+    JOptionPane.showMessageDialog(null, "RED WINS! We have a winner");
+   
+if (board.getConsecJumpNums() >= 25
+|| (!board.couldMove(PieceState.BLUE) && !board.couldMove(PieceState.BLUE))) {
+    if (board.getColorNums(PieceState.RED) > board.getColorNums(PieceState.BLUE)) {
+        JOptionPane.showMessageDialog(null, "RED WINS! We have a winner");
+    } else if (board.getColorNums(PieceState.RED) == board.getColorNums(PieceState.BLUE)) {
+        JOptionPane.showMessageDialog(null, "TIE End game but no one wins");
+    } else {
+        JOptionPane.showMessageDialog(null, "BLUE WINS! We have a winner String");
     }
+}
+}
+}
+  }
 
     @Override
     public void announceMove(Move move, PieceState player) {
-
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'announceMove'");
     }
+  
 
     @Override
     public void message(String format, Object... args) {
-
+        
+        JOptionPane.showMessageDialog(null, String.format(format, args));
     }
 
     @Override
     public void error(String format, Object... args) {
-
+        JOptionPane.showMessageDialog(null, "Simple Information Message");
+        JOptionPane.showMessageDialog(null, String.format(format, args), "Error", JOptionPane.ERROR_MESSAGE);
+        
     }
 
     public void setVisible(boolean b) {
@@ -55,5 +137,6 @@ class GUI implements View, CommandSource, Reporter {
     public void pack() {
 		
     }
-	
+
 }
+
--- Project/ataxx/View.java
+++ Project/ataxx/View.java
@@ -2,7 +2,6 @@ package ataxx;
 
 /** An updateable view of an Ataxx board. */
 interface View {
-
     /** Update the current view of the getAtaxxGame according to BOARD. */
     void update(Board board);
 
